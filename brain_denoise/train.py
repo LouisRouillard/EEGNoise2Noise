8d7a17ace7feb73740660c798b226579431896a0
Charlotte Caucheteux <ccaucheteux@devfair021.h1.fair>
2021-09-20 07:12:34 -0700

    nepoch

--- brain_denoise/train.py
+++ brain_denoise/train.py
@@ -1,21 +1,23 @@
 import torch
 
 # Train
-def train(dataloader, model, loss_fn, optimizer, device="cpu"):
+def train(dataloader, model, loss_fn, optimizer, n_epochs=10, device="cpu"):
     size = len(dataloader.dataset)
     model.train()
-    for batch, (X, y) in enumerate(dataloader):
-        X, y = X.to(device), y.to(device)
 
-        # Compute prediction error
-        pred = model(X)
-        loss = loss_fn(pred, y)
+    for epoch in range(n_epochs):
+        for batch, (X, y) in enumerate(dataloader):
+            X, y = X.to(device), y.to(device)
 
-        # Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            # Compute prediction error
+            pred = model(X)
+            loss = loss_fn(pred, y)
 
-        if batch % 100 == 0:
-            loss, current = loss.item(), batch * len(X)
-            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+            # Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            if batch % 100 == 0:
+                loss, current = loss.item(), batch * len(X)
+                print(f"epoch {epoch} \t loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
