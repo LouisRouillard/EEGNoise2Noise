68832c4d841c9b1a1f0e17e3da6a58760f846304
Charlotte Caucheteux <ccaucheteux@devfair021.h1.fair>
2021-09-22 02:16:21 -0700

    split id in utils

--- brain_denoise/main.py
+++ brain_denoise/main.py
@@ -7,18 +7,7 @@ import numpy as np
 from torch.nn import MSELoss
 from train import train as train_model
 from run_eval import run_eval
-
-
-def split_idx(n, splits=(0.6, 0.8, 1), shuffle=False):
-    idx = torch.arange(n)
-    if shuffle:
-        idx = np.random.permutation(idx)
-
-    train = idx[: int(splits[0] * n)]
-    valid = idx[int(splits[0] * n) : int(splits[1] * n)]
-    test = idx[int(splits[1] * n) : int(splits[2] * n)]
-    return train, valid, test
-
+from utils import split_idx
 
 if __name__ == "__main__":
     # Params
